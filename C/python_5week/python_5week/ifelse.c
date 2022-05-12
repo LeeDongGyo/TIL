b80df616739faf0f38d878d7cef8b030f25ef89e
LeeDongGyo <fnvp1234@naver.com>
2022-05-12 16:18:13 +0900

    python_5week 조건문

---
+++ C/python_5week/python_5week/ifelse.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+	//int i;
+	
+	//scanf("%d", i);
+
+	//if (i < 10) {
+	//	printf("10보다 작습니다.");
+	//	if (i < 5) {
+	//		printf("5보다 작습니다.");
+	//	}
+	//	else if (i == 1) {
+	//		printf("i는 1입니다. 야호\n");
+	//	}
+	//}
+	//else {
+	//	printf("10보다 큽니다.\n");
+	//}
+	//printf("신난다.");
+	//switch (i)
+	//{
+	//case 0: printf("0 입니다.\n");
+	//	break;
+	//case 1: printf("1 입니다.\n");
+	//	break;
+	//case 2: printf("2 입니다.\n");
+	//	break;
+	//case 3: printf("3 입니다.\n");
+	//	break;
+	//case 4: printf("4 입니다.\n");
+	//	break;
+	//case 5: printf("5 입니다.\n");
+	//	break;
+	//}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("신난다.\n");
+	}
+	return 0;
+}
---
+++ C/python_5week/python_5week/print1.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	//int i;
+	//puts("i 값을 입력해주세요");
+	//scanf("%d", &i);
+
+	//printf("%d", i);
+	//char i;
+	//puts("i 값을 입력해주세요");
+	//i = getchar();
+	//putchar(i);
+
+	//char i[99];
+	//puts("i 값을 입력해주세요");
+	//gets_s(i);
+	//puts(i);
+	
+	//char name[30];
+	//char sex;
+	//char age[99];
+
+	//printf("이름을 입력하세요: ");
+	//gets_s(name, 20);
+
+	//printf("성별을 입력하세요(M/W): ");
+	//sex = getchar();
+
+	//printf("생년월일을 입력하세요: ");
+	//gets_s(age, 20);
+
+	//printf("%s\n",name);
+	//putchar(sex);
+	//printf("\n");
+	//puts(age);
+	//return 0;
+}
